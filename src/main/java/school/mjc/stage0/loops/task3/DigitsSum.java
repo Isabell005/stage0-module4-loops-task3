b6a1206146a000e4726f00df064ac5b96bd5ca1a
Isabella Karibian <isabellakaribian@gmail.com>
2023-09-06 12:45:14 +0400

    stage0module4loopsTask3

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,19 +1,21 @@
 package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
-    public void printDigitsSum(int t) {
-        int  digit, sum =0;
 
-        while (t > 0) {
+    public static void main(String[] args) {
+        DigitsSum ds = new DigitsSum();
+        ds.printDigitsSum(12345); // Prints the sum of digits of the number 12345
+    }
 
-            digit = t % 10;
+    public void printDigitsSum(int number) {
+        int sum = 0;
 
-            sum = sum + digit;
-
-            t = t / 10;
+        while (number > 0) {
+            int digit = number % 10; // Get the last digit of the number
+            sum += digit; // Add the digit to the sum
+            number /= 10; // Remove the last digit from the number
         }
 
         System.out.println(sum);
-
     }
 }
